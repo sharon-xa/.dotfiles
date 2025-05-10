dab5098f0412070a82495f369b97df5c77f790f0
sharon <ali93456@gmail.com>
2025-05-10 03:05:05 +0300

    feat: add go, rust, sql, python language packs

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -2,5 +2,9 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.sql" }
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
 }
