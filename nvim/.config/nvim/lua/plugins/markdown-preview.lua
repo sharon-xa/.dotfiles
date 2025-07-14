74897d5390cc4900d35d6e815497590cabd84903
sharon <ali93456@gmail.com>
2025-07-14 21:54:20 +0300

    fix markdown preview installation

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,9 +1,7 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
+  build = "cd app && npm install",
+  init = function() vim.g.mkdp_filetypes = { "markdown" } end,
   ft = { "markdown" },
 }
