d7eb71ea8c626c4b005369a753266eb038b86a56
sharon <ali93456@gmail.com>
2025-08-21 04:25:33 +0300

    add the fish language pack

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -12,4 +12,5 @@ return {
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.fish" },
 }
